6ede695ae66a179ddc69042f3fda6059022c4de1
unc1efatty <734048451@qq.com>
2019-07-25 10:02:17 +0800

    ajax

---
+++ ajax/ajax.php
@@ -0,0 +1,4 @@
+<?php
+header(string:"content-type:text/html; charset=utf-8");
+echo file_get_contents( filename:"ajax.xml");
+?>
---
+++ own.php
@@ -0,0 +1,8 @@
+<?php
+    // print_r($_FILES["upFile"])
+    // echo "post page";
+//    echo $_GET["userName"];
+//    echo $_GET["userPwd"]
+ echo $_GET["userName"];
+ echo $_GET["userPwd"];
+?>
